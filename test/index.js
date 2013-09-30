583869734e3dbc261c621699098e97a28155ed0e
Tobias Muellerleile <muellerleile@hrz.uni-marburg.de>
2013-09-30 08:39:58 +0200

    Use short superagent test syntax

--- test/index.js
+++ test/index.js
@@ -5,10 +5,11 @@ var request = require('supertest');
 var app = require('../');
 var appConfig = app.get('config');
 
+request = request(app);
+
 suite('OPTIONS /', function () {
   test('return CORS headers', function (done) {
-    request(app)
-      .options('/')
+    request.options('/')
       .expect('Access-Control-Allow-Origin', '*')
       .expect('Access-Control-Allow-Headers', 'X-Requested-With')
       .expect('Access-Control-Allow-Methods', 'GET')
@@ -19,29 +20,26 @@ suite('OPTIONS /', function () {
 
 suite('GET /shorten', function () {
   test('checks for valid URL format', function (done) {
-    request(app)
-      .get('/shorten/http%3A%2F%2F')
+    request.get('/shorten/http%3A%2F%2F')
       .expect(400, done);
   });
 
   test('checks for valid protocol', function (done) {
-    request(app)
-      .get('/shorten/ftp%3A%2F%2Fwww.test.de%2Ffoo%2Fbar%2Fbaz')
+    request.get('/shorten/ftp%3A%2F%2Fwww.test.de%2Ffoo%2Fbar%2Fbaz')
       .expect(403, done);
   });
 
   test('refuses to shorten too short URLs', function (done) {
-    request(app)
-      .get('/shorten/http%3A%2F%2Ffoo.bar')
+    request.get('/shorten/http%3A%2F%2Ffoo.bar')
       .expect(403, done);
   });
 
   test('refuses request hashes when forbidden', function (done) {
     var requestHash = 'test';
     appConfig.hash.allowRequest = false;
-    request(app)
-      .get('/shorten/http%3A%2F%2Fa-very-long-domain.name%2Fwith%2Fan%2Feven' +
-        '%2Flonger%2Fpath%2Fand%2Fa%2Frequest%2Fhash?hash=' + requestHash)
+    request.get('/shorten/http%3A%2F%2Fa-very-long-domain.name' +
+        '%2Fwith%2Fan%2Feven%2Flonger%2Fpath%2Fand%2Fa%2Frequest%2Fhash?hash=' +
+        requestHash)
       .expect(403)
       .end(function (err, res) {
         appConfig.hash.allowRequest = true;
@@ -55,24 +53,23 @@ suite('GET /shorten', function () {
 
   test('refuses bad request hashes', function (done) {
     var requestHash = 'this%20is%20a%20malformed%20hash';
-    request(app)
-      .get('/shorten/http%3A%2F%2Fa-very-long-domain.name%2Fwith%2Fan%2Feven' +
-        '%2Flonger%2Fpath%2Fand%2Fa%2Frequest%2Fhash?hash=' + requestHash)
+    request.get('/shorten/http%3A%2F%2Fa-very-long-domain.name' +
+        '%2Fwith%2Fan%2Feven%2Flonger%2Fpath%2Fand%2Fa%2Frequest%2Fhash?hash=' +
+        requestHash)
       .expect(400, done);
   });
 
   test('refuses forbidden request hashes', function (done) {
     var requestHash = 'shorten';
-    request(app)
-      .get('/shorten/http%3A%2F%2Fa-very-long-domain.name%2Fwith%2Fan%2Feven' +
-        '%2Flonger%2Fpath%2Fand%2Fa%2Frequest%2Fhash?hash=' + requestHash)
+    request.get('/shorten/http%3A%2F%2Fa-very-long-domain.name' +
+        '%2Fwith%2Fan%2Feven%2Flonger%2Fpath%2Fand%2Fa%2Frequest%2Fhash?hash=' +
+        requestHash)
       .expect(400, done);
   });
 
   test('refuses URL with hostname on blacklist', function (done) {
     appConfig.urlLimit.hostnameBlacklist = ['a-bad-host.name'];
-    request(app)
-      .get('/shorten/http%3A%2F%2Fa-bad-host.name%2Fwith%2Fan%2Feven' +
+    request.get('/shorten/http%3A%2F%2Fa-bad-host.name%2Fwith%2Fan%2Feven' +
         '%2Flonger%2Fpath')
       .expect(403)
       .end(function (err, res) {
@@ -87,9 +84,8 @@ suite('GET /shorten', function () {
 
   test('refuses URL with hostname not on whitelist', function (done) {
     appConfig.urlLimit.hostnameWhitelist = ['a-known-good-host.name'];
-    request(app)
-      .get('/shorten/http%3A%2F%2Fanother-bad-host.name%2Fwith%2Fan%2Feven' +
-        '%2Flonger%2Fpath')
+    request.get('/shorten/http%3A%2F%2Fanother-bad-host.name' +
+        '%2Fwith%2Fan%2Feven%2Flonger%2Fpath')
       .expect(403)
       .end(function (err, res) {
         appConfig.urlLimit.hostnameWhitelist = [];
@@ -102,9 +98,8 @@ suite('GET /shorten', function () {
   });
 
   test('performs shortening', function (done) {
-    request(app)
-      .get('/shorten/http%3A%2F%2Fa-very-long-domain.name%2Fwith%2Fan%2Feven' +
-        '%2Flonger%2Fpath')
+    request.get('/shorten/http%3A%2F%2Fa-very-long-domain.name' +
+        '%2Fwith%2Fan%2Feven%2Flonger%2Fpath')
       .expect(200)
       .expect('Content-Type', /json/)
       .end(function (err, res) {
@@ -122,9 +117,8 @@ suite('GET /shorten', function () {
   });
 
   test('performs normalization during shortening', function (done) {
-    request(app)
-      .get('/shorten/http%3A%2F%2Fa-very-long-domain.name%2Fwith%2Fan%2Feven' +
-        '%2Flonger%2Fpath')
+    request.get('/shorten/http%3A%2F%2Fa-very-long-domain.name' +
+        '%2Fwith%2Fan%2Feven%2Flonger%2Fpath')
       .expect(200)
       .expect('Content-Type', /json/)
       .end(function (err, res) {
@@ -132,8 +126,7 @@ suite('GET /shorten', function () {
           return done(err);
         } else {
           var hashCanonicalUrl = res.body.hash;
-          request(app)
-            .get('/shorten/http%3A%2F%2Fa-very-long-domain.name%3A80' +
+          request.get('/shorten/http%3A%2F%2Fa-very-long-domain.name%3A80' +
                 '%2Fwith%2Fan%2Feven%2Flonger%2Fpath%23')
             .expect(200)
             .expect('Content-Type', /json/)
@@ -142,8 +135,7 @@ suite('GET /shorten', function () {
                 return done(err);
               } else {
                 assert.strictEqual(hashCanonicalUrl, res.body.hash);
-                request(app)
-                  .get('/shorten/http%3A%2F%2Fa-very-long-domain.name' +
+                request.get('/shorten/http%3A%2F%2Fa-very-long-domain.name' +
                       '%2Fwith%2Fan%2Feven%2Flonger%2Fpath%3F')
                   .expect(200)
                   .expect('Content-Type', /json/)
@@ -163,9 +155,9 @@ suite('GET /shorten', function () {
 
   test('accepts request hashes when allowed', function (done) {
     var requestHash = 'test';
-    request(app)
-      .get('/shorten/http%3A%2F%2Fa-very-long-domain.name%2Fwith%2Fan%2Feven' +
-        '%2Flonger%2Fpath%2Fand%2Fa%2Frequest%2Fhash?hash=' + requestHash)
+    request.get('/shorten/http%3A%2F%2Fa-very-long-domain.name' +
+        '%2Fwith%2Fan%2Feven%2Flonger%2Fpath%2Fand%2Fa%2Frequest%2Fhash?hash=' +
+        requestHash)
       .expect('Content-Type', /json/)
       .end(function (err, res) {
         if (err) {
@@ -178,18 +170,16 @@ suite('GET /shorten', function () {
   });
 
   test('returns text/plain (on demand)', function (done) {
-    request(app)
-      .get('/shorten/http%3A%2F%2Fa-very-long-domain.name%2Fwith%2Fan%2Feven' +
-        '%2Flonger%2Fpath?format=txt')
+    request.get('/shorten/http%3A%2F%2Fa-very-long-domain.name' +
+        '%2Fwith%2Fan%2Feven%2Flonger%2Fpath?format=txt')
       .expect('Content-Type', /text\/plain/)
       .expect(new RegExp('^' + appConfig.hash.urlPrefix))
       .expect(200, done);
   });
 
   test('returns qrcode (on demand)', function (done) {
-    request(app)
-      .get('/shorten/http%3A%2F%2Fa-very-long-domain.name%2Fwith%2Fan%2Feven' +
-        '%2Flonger%2Fpath?format=qrcode')
+    request.get('/shorten/http%3A%2F%2Fa-very-long-domain.name' +
+        '%2Fwith%2Fan%2Feven%2Flonger%2Fpath?format=qrcode')
       .expect('Content-Type', /image\/png/)
       .expect('Content-Length', /[0-9]{3,4}/)
       .expect(200, done);
@@ -200,15 +190,13 @@ suite('GET|HEAD /[hash]', function () {
   var hash;
 
   test('GET returns 404 for unknown hash', function (done) {
-    request(app)
-      .get('/unkno')
+    request.get('/unkno')
       .expect(404, done);
   });
 
   test('GET returns permanent redirect for known hash', function (done) {
-    request(app)
-      .get('/shorten/http%3A%2F%2Fa-very-long-domain.name%2Fwith%2Fan%2Feven' +
-        '%2Flonger%2Fpath%3F')
+    request.get('/shorten/http%3A%2F%2Fa-very-long-domain.name' +
+        '%2Fwith%2Fan%2Feven%2Flonger%2Fpath%3F')
       .expect(200)
       .expect('Content-Type', /json/)
       .end(function (err, res) {
@@ -216,8 +204,7 @@ suite('GET|HEAD /[hash]', function () {
           return done(err);
         } else {
           hash = res.body.hash;
-          request(app)
-            .get('/' + hash)
+          request.get('/' + hash)
             .expect('Location', 'http://a-very-long-domain.name/with/an/even' +
                 '/longer/path')
             .expect(301, done);
@@ -226,14 +213,12 @@ suite('GET|HEAD /[hash]', function () {
   });
 
   test('HEAD returns 404 for unknown hash', function (done) {
-    request(app)
-      .head('/unkno')
+    request.head('/unkno')
       .expect(404, done);
   });
 
   test('HEAD returns permanent redirect for known hash', function (done) {
-    request(app)
-      .head('/' + hash)
+    request.head('/' + hash)
       .expect('Location', 'http://a-very-long-domain.name/with/an/even' +
           '/longer/path')
       .expect(301, done);
@@ -244,15 +229,13 @@ suite('GET /expand', function () {
   var hash;
 
   test('returns 404 for unknown hash', function (done) {
-    request(app)
-      .get('/expand/unkno')
+    request.get('/expand/unkno')
       .expect(404, done);
   });
 
   test('returns long URL and hash for known hash', function (done) {
-    request(app)
-      .get('/shorten/http%3A%2F%2Fa-very-long-domain.name%2Fwith%2Fan%2Feven' +
-        '%2Flonger%2Fpath%3F')
+    request.get('/shorten/http%3A%2F%2Fa-very-long-domain.name' +
+        '%2Fwith%2Fan%2Feven%2Flonger%2Fpath%3F')
       .expect(200)
       .expect('Content-Type', /json/)
       .end(function (err, res) {
@@ -260,8 +243,7 @@ suite('GET /expand', function () {
           return done(err);
         } else {
           hash = res.body.hash;
-          request(app)
-            .get('/expand/' + hash)
+          request.get('/expand/' + hash)
             .expect('Content-Type', /json/)
             .expect(200)
             .end(function (err, res) {
@@ -279,8 +261,7 @@ suite('GET /expand', function () {
   });
 
   test('returns long URL in text/plain (on demand)', function (done) {
-    request(app)
-      .get('/expand/' + hash + '?format=txt')
+    request.get('/expand/' + hash + '?format=txt')
       .expect('Content-Type', /text\/plain/)
       .expect('http://a-very-long-domain.name/with/an/even/longer/path')
       .expect(200, done);
@@ -292,15 +273,13 @@ suite('GET /stats', function () {
   var hash;
 
   test('returns 404 for unknown hash', function (done) {
-    request(app)
-      .get('/stats/unkno')
+    request.get('/stats/unkno')
       .expect(404, done);
   });
 
   test('returns stats for known hash', function (done) {
-    request(app)
-      .get('/shorten/http%3A%2F%2Fa-very-long-domain.name%2Fwith%2Fan%2Feven' +
-        '%2Flonger%2Fpath%3F')
+    request.get('/shorten/http%3A%2F%2Fa-very-long-domain.name' +
+        '%2Fwith%2Fan%2Feven%2Flonger%2Fpath%3F')
       .expect(200)
       .expect('Content-Type', /json/)
       .end(function (err, res) {
@@ -308,8 +287,7 @@ suite('GET /stats', function () {
           return done(err);
         } else {
           hash = res.body.hash;
-          request(app)
-            .get('/stats/' + hash)
+          request.get('/stats/' + hash)
             .expect('Content-Type', /json/)
             .expect(200, done);
         }
@@ -317,8 +295,7 @@ suite('GET /stats', function () {
   });
 
   test('returns clicks in text/plain (on demand)', function (done) {
-    request(app)
-      .get('/stats/' + hash + '?format=txt')
+    request.get('/stats/' + hash + '?format=txt')
       .expect('Content-Type', /text\/plain/)
       .expect(/\d+/)
       .expect(200, done);
@@ -327,8 +304,7 @@ suite('GET /stats', function () {
   test.skip('returns actually incremented clicks', function (done) {
     this.slow(4075);
     this.timeout(5000);
-    request(app)
-      .get('/stats/' + hash + '?format=txt')
+    request.get('/stats/' + hash + '?format=txt')
       .expect('Content-Type', /text\/plain/)
       .expect(200)
       .end(function (err, res) {
@@ -337,12 +313,10 @@ suite('GET /stats', function () {
         } else {
           var clicks = parseInt(res.text, 10);
           setTimeout(function () {
-            request(app)
-              .get('/' + hash)
+            request.get('/' + hash)
               .expect(301)
               .end(function (err, res) {
-                request(app)
-                  .get('/stats/' + hash + '?format=txt')
+                request.get('/stats/' + hash + '?format=txt')
                   .expect(200)
                   .expect('' + (clicks + 1), done);
               });
